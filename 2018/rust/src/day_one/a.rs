7627baec29f2ede44f698833d3acb781483cc3a5
Rasmus Eneman <rasmus@eneman.eu>
2018-12-01 21:37:19 +0100

    use sum method

--- 2018/rust/src/day_one/a.rs
+++ 2018/rust/src/day_one/a.rs
@@ -1,6 +1,5 @@
 use std::fs::File;
 use std::io::prelude::*;
-use std::ops::Add;
 
 pub fn run() {
     let mut f = File::open("src/day_one/a.input").expect("File not found");
@@ -9,11 +8,11 @@ pub fn run() {
     f.read_to_string(&mut contents)
         .expect("Something went wrong reading the file");
 
-    let val = contents
+    let val: i32 = contents
       .split("\n")
       .filter(|row| !row.is_empty())
       .map(|row| row.parse::<i32>().expect("Value not number?"))
-      .fold(0, Add::add);
+      .sum();
 
     println!("Value: {}", val);
 }
